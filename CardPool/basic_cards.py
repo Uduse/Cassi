c4ca3ba8a7471bdc61e433f32936e730c696c93c
ranxu95 <ranxu@ucdavis.edu>
2016-09-12 21:37:21 -0700

    ran xu stronger!!

--- CardPool/basic_cards.py
+++ CardPool/basic_cards.py
@@ -75,7 +75,7 @@ class Guard(Creature):
 
 class RanXu(Creature):
     def __init__(self, owner=None):
-        super().__init__("Ran Xu", 1, 1, owner)
+        super().__init__("Ran Xu", 2, 1, owner)
 
     def cost(self):
         return 20
